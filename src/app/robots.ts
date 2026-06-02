fb49106bf3068d88db0167872dc0f3a675f961c8
tranhuyland <huy07kt@gmail.com>
2026-06-02 10:22:35 +0700

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,11 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: '*', allow: '/', disallow: '/api/' },
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/api/',
+    },
     sitemap: 'https://tranhuyland.vn/sitemap.xml',
   };
 }
