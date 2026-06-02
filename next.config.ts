62a242a925d454b1ee728de7274501ae8a111b7f
tranhuyland <huy07kt@gmail.com>
2026-06-02 11:37:42 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "i.postimg.cc" },
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "maps.google.com" }
+      { protocol: "https", hostname: "images.unsplash.com" }
     ],
   },
 };
