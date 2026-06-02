944d4cb79c5da43eb379e12bccaa2e1d4755de0e
tranhuyland <huy07kt@gmail.com>
2026-06-02 10:48:36 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
       </head>
-      <body class="antialiased min-h-screen flex flex-col pb-20 md:pb-0">
+      <body className="antialiased min-h-screen flex flex-col pb-20 md:pb-0">
         {children}
       </body>
     </html>
