f0ea622555b068ba7309a20eea4c7f149b808a2f
tranhuyland <huy07kt@gmail.com>
2026-06-08 11:39:12 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "i.postimg.cc" },
-      { protocol: "https", hostname: "images.unsplash.com" }
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "res.cloudinary.com" } // Đã tích hợp domain Cloudinary để load ảnh mượt mà
     ],
   },
 };
